e1e190c542dbba4c8966fdf3c44ede3b633f9dd2
unknown <rafiqkhan8971@gmail.com>
2025-01-22 16:37:25 +0530

    issue Solved

--- backend/app.js
+++ backend/app.js
@@ -2,22 +2,24 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
-const galleryRoutes = require('./backend/routes/galleryRoutes');
-const productRoutes = require('./backend/routes/productRoutes');
+const galleryRoutes = require('./routes/galleryRoutes.js');
+const productRoutes = require('./routes/productRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Static files
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use('/assets', express.static(path.join(__dirname, '../frontend/assets')));
+
 
 // API routes
 app.use('/api/gallery', galleryRoutes);
 app.use('/api/products', productRoutes);
 
+
 // Home Route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 // Start server
