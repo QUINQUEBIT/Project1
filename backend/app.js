86c750e7a3a00de6807a11fdd264abf3fcee218c
Likhitha A S <133338541+Likhitha310@users.noreply.github.com>
2025-01-24 11:43:45 +0530

    small changes

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,27 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, '../frontend/views'));
 
+// Serve EJS Pages
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.get("/about-us", (req, res) => {
+  res.render("about-us", { title: "About Us" });
+});
+
+app.get("/services", (req, res) => {
+  res.render("services", { title: "Services" });
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects", { title: "Projects" });
+});
+
+app.get("/products", (req, res) => {
+  res.render("products", { title: "Products" });
+});
+
 app.get('/', (req, res) => {
   res.render('index');
 });
