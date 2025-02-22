690afd5f4a0263074479548538e8c28f87b6f392
rafbot-v7 <rafiqkhan8971@gmail.com>
2025-02-22 22:41:30 +0530

    animation

--- frontend/assets/js/scripts.js
+++ frontend/assets/js/scripts.js
@@ -2,6 +2,63 @@
 let currentIndex = 0;
 const slides = document.querySelectorAll('.slider img');
 const totalSlides = slides.length;
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+  reset: false
+});
+
+ScrollReveal().reveal(".title", {
+  duration: 1000,
+  origin: "top",
+  distance: "400px",
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  rotate: {
+    x: 20,
+    z: -10
+  }
+});
+
+ScrollReveal().reveal(".fade-in", {
+  duration: 2000,
+  move: 0
+});
+
+ScrollReveal().reveal(".scaleUp", {
+  duration: 500,
+  scale: 0.85
+});
+
+ScrollReveal().reveal(".flip", {
+  delay: 500,
+  duration: 2000,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+
+ScrollReveal().reveal(".slide-right", {
+  duration: 500,
+  origin: "left",
+  distance: "300px",
+  easing: "ease-in-out"
+});
+ScrollReveal().reveal(".slide-left", {
+  duration: 500,
+  origin: "right",
+  distance: "300px",
+  easing: "ease-in-out"
+});
+
+ScrollReveal().reveal(".slide-up", {
+  duration: 500,
+  origin: "bottom",
+  distance: "100px",
+  easing: "cubic-bezier(.37,.01,.74,1)",
+  opacity: 0.3,
+  scale: 0.5
+});
 
 setInterval(() => {
   slides[currentIndex].style.transform = `translateX(-100%)`;
